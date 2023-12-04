6ab3fc5ff44e30366bfad3b57c106515350986ca
Fonta97 <fontana.alessandro97@gmail.com>
2023-12-04 17:29:11 +0100

    feat: added comments

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
+
 const gridElement = document.getElementById("grid");
 let output = 0
 
+// variabile bombe generata con 16 numeri casuali dalla funzione genArrayRandomNum
 const bombe = genArrayRandomNum(1, 100, 16);
 console.log(bombe);
+
+// creazione variabile punteggio
 let score = 0;
 console.log("punteggio: ", score);
 
+// ciclo for in cui creo le caselle e controllo se clicco sulle bombe 
 for (let i = 0; i < 100; i++) {
 
     const newElement = createMyElement("div", "square");
@@ -18,11 +23,15 @@ for (let i = 0; i < 100; i++) {
         function () {
             this.classList.add("clicked")
             console.log("clicked");
+
+            // controllo se bombe include output che ho cliccato
             if (bombe.includes(output)) {
                 this.classList.add("clickedboom")
+                // messaggio in console con fine gioco e punteggio (con l'alert non funziona)
                 console.log("boom fine gioco");
                 console.log("hai totalizato" + score)
             } else {
+                // alternativamente se il valore di bomba non è in quello che ho cliccato aumenta il punteggio e colora diversamente la casella
                 this.classList.add("clicked")
                 score++;
             }
@@ -34,7 +43,7 @@ for (let i = 0; i < 100; i++) {
 
 }
 
-
+// funzione di creazione elemento div e nome classe
 function createMyElement(tagtype, classname) {
 
     const currentElement = document.createElement(tagtype);
@@ -43,7 +52,7 @@ function createMyElement(tagtype, classname) {
     return currentElement;
 }
 
-// bomb generator
+//funzione di generatore caselle di appartenenza delle bombe in base alla lunghezza dell'array con possibile implementazione della difficoltà
 
 function genArrayRandomNum(minNum, maxNum, lunghezzaArr) {
 
